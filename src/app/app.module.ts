d8df08e9cd238e9a0cada6bc0599b4db0ba4d0c5
ivankoretskyy <ivankoretskyy@gmail.com>
2018-08-30 02:31:25 +0300

    add store for navigateion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,19 @@ import { MainModule } from "./main/main.module";
 import { StoreModule } from "@ngrx/store";
 import { RouterModule } from "@angular/router";
 import { CoreModule } from "./core/core.module";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "../environments/environment.prod";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      name: "vidly App",
+      maxAge: 25,
+      logOnly: environment.production
+    }),
     MainModule,
     RouterModule.forRoot([
       { path: "users", loadChildren: "./user/user.module#UserModule" },
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,25 +1,39 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Store, select } from "@ngrx/store";
+import { filter, takeWhile } from "rxjs/operators";
 
 @Component({
   selector: "vidly-navigation",
   templateUrl: "./navigation.component.html",
   styleUrls: ["./navigation.component.css"]
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements OnInit, OnDestroy {
   showDashboard = false;
   currentUser;
+  componentAlive = true;
   constructor(private store: Store<any>) {}
 
   ngOnInit() {
-    this.store.pipe(select("users")).subscribe(state => {
-      if (state && state.currentUser) {
-        debugger;
-        this.currentUser = state.currentUser;
-      }
-    });
+    this.setCurrentUser();
   }
 
+  ngOnDestroy() {
+    this.componentAlive = false;
+  }
+
+  setCurrentUser() {
+    this.store
+      .pipe(
+        select("users"),
+        filter(i => i),
+        takeWhile(() => this.componentAlive)
+      )
+      .subscribe(state => {
+        if (state && state.currentUser) {
+          this.currentUser = state.currentUser;
+        }
+      });
+  }
   toggleProjects() {
     this.showDashboard = !this.showDashboard;
     this.store.dispatch({
